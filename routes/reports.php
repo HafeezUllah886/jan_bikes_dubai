4e2394923aab89576ae576d289a47cc25e5c2710
HafeezUllah886 <Hafeezullah886@gmail.com>
2026-07-17 00:27:23 +0500

    done

--- routes/reports.php
+++ routes/reports.php
@@ -2,10 +2,9 @@
 
 use App\Http\Controllers\reports\ledgerReportController;
 use App\Http\Controllers\reports\profitLossReportController;
-use App\Http\Middleware\adminCheck;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth', adminCheck::class)->group(function () {
+Route::middleware('auth')->group(function () {
 
     Route::get('/reports/ledger', [ledgerReportController::class, 'index'])->name('reportLedger');
     Route::get('/reports/ledgerData', [ledgerReportController::class, 'reportData'])->name('reportLedgerData');
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,16 @@
 use App\Http\Controllers\dashboardController;
 use Illuminate\Support\Facades\Route;
 
-require __DIR__ . '/auth.php';
-require __DIR__ . '/finance.php';
-require __DIR__ . '/purchase.php';
-require __DIR__ . '/imports.php';
-require __DIR__ . '/reports.php';
-require __DIR__ . '/sales.php';
-require __DIR__ . '/setups.php';
-require __DIR__ . '/stock.php';
-
+require __DIR__.'/auth.php';
+require __DIR__.'/finance.php';
+require __DIR__.'/purchase.php';
+require __DIR__.'/imports.php';
+require __DIR__.'/reports.php';
+require __DIR__.'/sales.php';
+require __DIR__.'/setups.php';
+require __DIR__.'/stock.php';
 
 Route::middleware('auth')->group(function () {
 
     Route::get('/', [dashboardController::class, 'index'])->name('dashboard');
 });
-
-
